9cb5ee5c3dbed158794b1aa78bc369d7b1636998
MartinSorin <martin.sorin2022@campus-eni.fr>
2022-05-31 12:14:01 +0200

    refactor repository

--- src/Entity/Trip.php
+++ src/Entity/Trip.php
@@ -2,12 +2,12 @@
 
 namespace App\Entity;
 
-use App\Repository\SortieRepository;
+use App\Repository\TripRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
-#[ORM\Entity(repositoryClass: SortieRepository::class)]
+#[ORM\Entity(repositoryClass: TripRepository::class)]
 class Trip
 {
     #[ORM\Id]
--- src/Repository/TripRepository.php
+++ src/Repository/TripRepository.php
@@ -14,7 +14,7 @@ use Doctrine\Persistence\ManagerRegistry;
  * @method Trip[]    findAll()
  * @method Trip[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
  */
-class SortieRepository extends ServiceEntityRepository
+class TripRepository extends ServiceEntityRepository
 {
     public function __construct(ManagerRegistry $registry)
     {
