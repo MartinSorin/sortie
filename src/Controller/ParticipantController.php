0b853635b27a0a7bf83df0f2dd5ee7d3bd0ce10f
Maxence Chotard <maxence.chotard2022@campus-eni.fr>
2022-05-31 16:14:20 +0200

    page enregistrement des participants

--- src/Controller/ParticipantController.php
+++ src/Controller/ParticipantController.php
@@ -2,17 +2,36 @@
 
 namespace App\Controller;
 
+use App\Entity\Participant;
+use App\Repository\CampusRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Form\ParticpantType;
+
 
 class ParticipantController extends AbstractController
 {
-    #[Route('/participant', name: 'app_participant')]
-    public function index(): Response
+
+    #[Route('/updateProfil', name: 'updateProfil')]
+    public function updateProfile(): Response
     {
-        return $this->render('participant/index.html.twig', [
-            'controller_name' => 'ParticipantController',
-        ]);
+
+        $participant = new Participant ();
+        $participant->setName('');
+        $participant->setFirstname('');
+        $participant->setPhone('');
+        $participant->setEmail('');
+        $participant->setPassword('');
+
+
+        $form = $this->createForm(ParticpantType::class, $participant);
+
+        return $this->render('participant/updateProfil.html.twig', array(
+            'form' => $form->createView(),
+        ));
     }
+
+
+
 }
---
+++ src/Form/ParticpantType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Participant;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+
+class ParticpantType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name',null, ['label' => 'Nom'] )
+            ->add('firstname',null, ['label' => 'Prénom'])
+            ->add('phone',null, ['label' => 'Téléphone'])
+            ->add('email',null, ['label' => 'Email'])
+            ->add('password',null, ['label' => 'Mot de passe'])
+            ->add('isAffectedTo', EntityType::class, ['label' => 'Campus', 'choice_label' => 'name', 'class' => 'App\Entity\Campus',
+        'required' => false
+    ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Participant::class,
+        ]);
+    }
+}
