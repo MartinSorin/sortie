f2c99b35bb85248b9d3e858aad81589611b697fa
MartinSorin <martin.sorin2022@campus-eni.fr>
2022-05-31 17:08:38 +0200

    push mardi soir

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -11,7 +11,7 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class MainController extends AbstractController
 {
-    #[Route('/', name: 'home')]
+    #[Route('/home', name: 'home')]
     public function home(TripRepository $tripRepository, CampusRepository $campusRepository): Response
     {
         $trips = $tripRepository->findAll();
