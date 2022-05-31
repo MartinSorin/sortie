e5dd9cffc3812964e4a89eb6cfd540d936cabf22
MartinSorin <martin.sorin2022@campus-eni.fr>
2022-05-31 15:21:49 +0200

    Accueil with form and table

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,17 +2,27 @@
 
 namespace App\Controller;
 
+use App\Form\FilterType;
+use App\Repository\CampusRepository;
+use App\Repository\TripRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MainController extends AbstractController
 {
-    #[Route('/home', name: 'home')]
-    public function home(): Response
+    #[Route('/', name: 'home')]
+    public function home(TripRepository $tripRepository, CampusRepository $campusRepository): Response
     {
-        return $this->render('main/home.html.twig'[
+        $trips = $tripRepository->findAll();
+        $campus = $campusRepository->findAll();
 
+        $form = $this->createForm(FilterType::class);
+
+        return $this->render('main/home.html.twig', [
+            'trips' => $trips,
+            'campusList' => $campus,
+            'form' => $form->createView()
             ]);
     }
 }
