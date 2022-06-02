fe547b5055d2606ecc1679f502311d30df22cb17
MartinSorin <martin.sorin2022@campus-eni.fr>
2022-06-02 17:03:57 +0200

    Jeudi soir

---
+++ src/Form/PlaceType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Place;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class PlaceType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name', null, ['label' => 'Nom', 'required' => false])
+            ->add('street', null, ['label' => 'Rue', 'required' => false])
+            ->add('latitude', null, ['label' => 'Latitude', 'required' => false])
+            ->add('longitude', null, ['label' => 'Longitude', 'required' => false])
+            ->add('city', EntityType::class, ['label' => 'Ville', 'choice_label' => 'name', 'class' => 'App\Entity\City'])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Place::class,
+        ]);
+    }
+}
